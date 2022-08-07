26ea25317442a48ac44f8c196d227ed1d118e904
Bugalete Daniel <bugaletedaniel1@gmail.com>
2022-08-07 22:35:29 +0300

    animations and burger menu

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ function animatedSlide(){
 
     })
     .setTween(slideTl)
-    .addIndicators({
-        colorStart:'white', 
-        colorTrigger:'white', 
-        name:'slide'
-    })
+    // .addIndicators({
+    //     colorStart:'white', 
+    //     colorTrigger:'white', 
+    //     name:'slide'
+    // })
     .addTo(controller);
     //new Animation
     const pageTl = gsap.timeline();
@@ -48,15 +48,64 @@ function animatedSlide(){
         duration: '100%',
         triggerHook: 0
     })
-    .addIndicators({
-        colorStart:'white', 
-        colorTrigger:'white', 
-        name:'page',
-        indent: 200
-    })
+    // .addIndicators({
+    //     colorStart:'white', 
+    //     colorTrigger:'white', 
+    //     name:'page',
+    //     indent: 200
+    // })
     .setPin(slide, { pushFollowers:false })
     .setTween(pageTl)
     .addTo(controller)
 });
 };
+const mouse = document.querySelector('.cursor');
+const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
+
+function cursor(e) {
+    mouse.style.top = e.pageY + 'px';
+    mouse.style.left = e.pageX + 'px'
+}
+function activeCursor(e) {
+    const item = e.target;
+    if(item.id === 'logo' || item.classList.contains('burger')){
+        mouse.classList.add('nav-active')
+    }else{
+        mouse.classList.remove('nav-active')
+    }
+    if(item.classList.contains('explore')){
+        mouse.classList.add('explore-active');
+        gsap.to('.title-swipe', 1, { y: '0%'})
+        mouseTxt.innerText = 'Tap';
+    }else{
+        mouse.classList.remove('explore-active');
+        gsap.to('.title-swipe', 1, { y: '100%'})
+        mouseTxt.innerText = '';
+    }
+}
+
+
+function navToggle(e) {
+    if (!e.target.classList.contains('active')){
+        e.target.classList.add('active')
+        gsap.to('.line1', 0.5, { rotate:'45', y:  5, background: 'black', zIndex: '100' } )
+        gsap.to('.line2', 0.5, { rotate:'-45', y:-5, background: 'black', zIndex: '100' } )
+        gsap.to('.nav-bar', 2, { clipPath: 'circle(2500px at 100% -10%)' } )
+        gsap.to('#logo',1, {color:'black'})
+        document.body.classList.add('hide')
+    }else{
+        e.target.classList.remove('active')
+        gsap.to('.line1', 0.5, { rotate:'0', y: 0, background: 'white', } )
+        gsap.to('.line2', 0.5, { rotate:'0', y: 0, background: 'white', } )
+        gsap.to('.nav-bar', 2, { clipPath: 'circle(25px at 100% -10%)' } )
+        gsap.to('#logo',1, {color:'white'})
+        document.body.classList.remove('hide')
+    }
+}
+
+// event listeners
+burger.addEventListener('click', navToggle)
+window.addEventListener('mousemove', cursor);
+window.addEventListener('mouseover', activeCursor)
 animatedSlide();
